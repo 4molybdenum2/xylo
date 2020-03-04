e87010c8444ae854b0d62942b7cb8205ed7c7699
4molybdenum2 <tathagatapaul7@gmail.com>
2020-03-04 20:34:38 +0530

    fixed animations

--- public/js/app.js
+++
@@ -1,7 +0,0 @@
-let vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty('--vh', '${vh}px');
-
-const landtext = document.querySelector('landing-text');
-const discover = document.querySelector('button');
-const iconbar = documetn.querySelector('icon-bar');
-
