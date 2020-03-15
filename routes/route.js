12fd9119384290671fe647adee87dab150769cad
4molybdenum2 <tathagatapaul7@gmail.com>
2020-03-15 12:37:05 +0530

    made changes

--- routes/route.js
+++ routes/route.js
@@ -8,4 +8,8 @@ router.get("/about", (req, res) => {
 router.get("/homepage", (req, res) => {
   res.render("index");
 });
+
+router.get("/database",(req,res)=>{
+  res.send("Database");
+});
 module.exports = router;
