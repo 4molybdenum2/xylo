51e27137e41fc8710110543612dbb98ed6e3b34c
4molybdenum2 <tathagatapaul7@gmail.com>
2020-03-19 02:06:18 +0530

    added db functionality for posts

--- routes/route.js
+++ routes/route.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const connection = require('../db/db');
 
 router.get("/", (req, res) => {
   res.render("index");
 });
 
-router.get("/stories" , (req,res) => {
-  res.render("stories");
+router.get("/stories/:id" , (req,res) => {
+  // console.log(res);
+  var sql = 'SELECT * FROM posts';
+  connection.query(sql , (err , rows , fields)=>{
+      if(err) throw err;
+      else{
+          var urlid = req.params.id;
+          console.log('query successful');
+          obj = {print:[rows[urlid]]};
+          // var imageUrl = obj.print[0].image;
+          console.log(obj);
+          // console.log(imageUrl);
+          res.render('stories',{obj:obj});
+      }
+  });
 });
 
 module.exports = router;
