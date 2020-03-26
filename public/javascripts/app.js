2369b8882d4f9980c3325a3c187621954486a75d
Pranshu Saxena <pranshutejas@gmail.com>
2020-03-26 19:53:08 +0530

    Styling changes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,10 +1,8 @@
-// let vh = window.innerHeight * 0.01;
-//         document.documentElement.style.setProperty('--vh', '${vh}px');
-
-
-window.addEventListener('load',()=>{
-        setTimeout(function(){
-                const preloader = document.querySelector('.preloader');
-        preloader.classList.add('preload-finish');
-        }, 500);
+window.addEventListener("load", () => {
+  setTimeout(function() {
+    const body = document.getElementById("body");
+    const preloader = document.querySelector(".preloader");
+    body.style.overflow="initial";
+    preloader.classList.add("preload-finish");
+  }, 500);
 });
